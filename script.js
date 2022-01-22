92284bd2038e2e4f63b4be2661ddb776b42cf142
Pampulina <92136365+Pampulina@users.noreply.github.com>
2022-01-22 18:39:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+
+const links = document.querySelectorAll('.header-menu__item a')
+
+seamless.polyfill();
+
+links.forEach((element) => {
+ element.addEventListener('click', (event) =>{
+    event.preventDefault()
+
+    const id=element.getAttribute ('href').substring(1)
+    const section = document.getElementById(id)
+    
+    if (section) {
+       seamless.elementScrollIntoView(section, {
+         behavior: "smooth",
+         block: 'start',
+       });
+   } else {
+      seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+        behavior: "smooth",
+        block: "center",
+        inline: "center",
+      });
+      console.log('нет');
+   }
+ })
+})
+const link = document.querySelectorAll(".card-details__link-characteristics")
+seamless.polyfill();
+
+link.forEach((element) => {
+   element.addEventListener('click', (event) =>{
+      event.preventDefault()
+     
+      const id = element.getAttribute('href').substring(1)
+      const section = document.getElementById(id)
+      if (section) {
+         seamless.elementScrollIntoView(section, {
+           behavior: "smooth",
+           block: "start",
+         });
+      } else {
+          seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+            behavior: "smooth",
+            block: "center",
+            inline: "center",
+          });
+      }
+   })
+})
+
+
+
+
+
+
